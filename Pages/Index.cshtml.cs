c4a1be4da16bc82e1080558545b2be12fc95dfd9
Fredrik <fredrik.t.bengtsson@hotmail.com>
2026-07-09 15:43:14 +0200

    adding base stuff

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SciFiPortfolio.Pages
@@ -7,7 +6,7 @@ namespace SciFiPortfolio.Pages
     {
         public void OnGet()
         {
-
+            ViewData["Title"] = "Fredrik Bengtsson | .NET Developer Portfolio";
         }
     }
 }
